4d183e22c580a69a8c891d0eb87f501672648857
Mr. Royal <82133758+gxurxv@users.noreply.github.com>
2022-09-07 08:18:17 +0530

    Update song.py

--- Ankit/modules/song.py
+++ Ankit/modules/song.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: [ᴀɴᴋɪᴛ 🇮🇳](t.me/XnKiTKuMaR)'
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: [ᴘʀᴏᴊᴇᴄᴛ-x](github.com/nvs-official)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
